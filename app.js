b0855f3c114e0e328fe1d9eea6f6251c982a6f8f
Ayubxon <aubsattorov@gmail.com>
2025-02-21 12:24:58 +0500

    homeWork end

---
+++ app.js
@@ -0,0 +1,74 @@
+// const hh = document.querySelectorAll(["h1", "h2", "div"]);
+
+// console.log(hh);
+
+// const id = document.getElementById("div");
+// const classsName = document.getElementsByClassName("salom");
+// const tagNmae = document.getElementsByTagName("h2");
+
+// const all = [classsName, id, tagNmae];
+
+// console.log(all);
+
+// uyga vazifa 1
+
+// nodelist bn HTMLcolectiondi farqi topib kelish
+
+// console.log(div);
+
+// const all = document.querySelector("h2");
+
+// console.log(all);
+
+// const doc = (document.title = "men uchun");
+// const doc = document.forms;
+
+// console.log(doc);
+// innderHTML
+// document.title
+// document.forms
+// document.images;
+
+// const h1 = document.createElement("h1");
+// h1.innerHTML = "nafrat";
+// console.log(h1);
+
+// document.body.append(h1);
+
+//uyga vazifa 2
+// innerText xamda TextContentni faqtini topish
+
+// const creat = document.createElement("h1");
+// const creat1 = document.createElement("h1");
+// const creat2 = document.createElement("h2");
+// const creat3 = document.createElement("h2");
+// const creat4 = document.createElement("h2");
+// const creat5 = document.createElement("span");
+// const creat6 = document.createElement("span");
+// const creat7 = document.createElement("span");
+// const creat8 = document.createElement("span");
+
+// const all = [
+//   creat,
+//   creat1,
+//   creat2,
+//   creat3,
+//   creat4,
+//   creat5,
+//   creat6,
+//   creat7,
+//   creat8,
+// ];
+
+// console.log(all);
+
+// const elOl = document.getElementById("ol");
+// const h1 = document.createElement("h1");
+// h1.textContent = "Men qoshilgan elementman";
+
+// elOl.prepend(h1);
+
+// .after
+// .befor
+// .append
+// .prepend
---
+++ index.js
@@ -0,0 +1,52 @@
+// 1-savol HTMLCollection hamda NodeList o’rtasidagi 2 ta farq
+// const divs = document.getElementsByTagName("div");
+// const divs2 = document.querySelectorAll("div");
+// console.log(divs);
+// console.log(divs2);
+// HTMLColectionda forEach() ishlatib bolmas ekan
+// NodeListda forEach() ni ishlatsa boladi
+// const nodes = document.querySelectorAll("p");
+// nodes.forEach((node) => {
+//   console.log(node.textContent);
+// });
+
+// 2-savol innerText hamda textContent o’rtasidagi 2 ta farq
+// Innertext ekranda korngan matndi oladi
+// textContent barcha matndi oladi display none bolsaxam oladi
+
+// 3-savol DOM nma
+// DOM Document Object Model
+// JavaScript yordamida elementlar qoshish ozgartirish ochirish mumkin
+
+// 4-savol 30 ta event nomini hamda vazifasini yozing.
+
+// 1 click bosilganda
+// 2 dblclick Ikki marta bosilganda
+// 3 mousedown mishka bosilganda
+// 4 mouseup mishka tugmasini qoyib yuborilganda
+// 5 mouseover mishka hover
+// 6 mouseout mishka berilganm obyectdan chiqib ketganida
+// 7 mousemove mishka xarakatlanganda
+// 8 keydown klaviatura tugmasi bosilganda
+// 9 keyup tugma qoyib yuborilganda
+// 10 keypress Tugma bosilganda
+// 11 input input maydoniga yozilganda
+// 12 change select o‘zgarishida
+// 13 focus inputga kursor kirganda
+// 14 blur inputdan kursor chiqqanda
+// 15 submit Form jonatilganda
+// 16 reset Form tozalanganda
+// 17 load HTML yuklanganda
+// 18 unload HTML yopilganda
+// 19 resize Oyna hajmi o'zgarganda
+// 20 scroll HTML skroll qilinganda
+// 21 contextmenu O'ng tugma bosilganda
+// 22 touchstart Sensor ekranga tegilganda
+// 23 touchend touchbar qo'l olinanda
+// 24 touchmove touchbar harakatlantirilganda
+// 25 drag Obyekt sudraganda
+// 26 dragstart surish boshlanganda
+// 27 dragend surish tugaganda
+// 28 drop Obyekt tashlanganda
+// 29 copy nusxa olganda
+// 30 paste nusxa qoyilganda
